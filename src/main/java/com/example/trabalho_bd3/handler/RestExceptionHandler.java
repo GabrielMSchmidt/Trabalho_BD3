5fddf36b9474cbda04fab2e330b8b08ecb776221
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2024-06-10 11:10:26 -0300

    Finalização do CRUD de Usuarios

--- src/main/java/com/example/trabalho_bd3/handler/RestExceptionHandler.java
+++ src/main/java/com/example/trabalho_bd3/handler/RestExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import com.example.trabalho_bd3.common.ConversorData;
 import com.example.trabalho_bd3.domain.exception.BadRequestException;
